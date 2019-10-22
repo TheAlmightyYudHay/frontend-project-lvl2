445688a18bc35278bd25ed4042d86fb0d44ed9ba
Anton Kulish <antonsergeevichkulish@gmail.com>
2019-10-22 08:19:29 +0300

    Added functionality for parse ini files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,22 +2,11 @@ import path from 'path';
 import plainDiff from './__fixtures__/plainDiff';
 import genDiff from '../src/genDiff';
 
-test('should compare two plain JSON\'s', () => {
-  const previousFile = path.join(__dirname, '/__fixtures__/', 'diff1Plain.json');
-  const actualFile = path.join(__dirname, '/__fixtures__/', 'diff2Plain.json');
-  expect(genDiff(previousFile, actualFile)).toBe(plainDiff);
-});
-
-describe('should compare two plain YAML\'s', () => {
-  test('should work with .yaml extension', () => {
-    const previousFile = path.join(__dirname, '/__fixtures__/', 'diff1Plain.yaml');
-    const actualFile = path.join(__dirname, '/__fixtures__/', 'diff2Plain.yaml');
+test.each([['json'], ['yaml'], ['yml'], ['ini']])(
+  'should compare to %s\'s files',
+  (extension) => {
+    const previousFile = path.join(__dirname, '/__fixtures__/', `diff1Plain.${extension}`);
+    const actualFile = path.join(__dirname, '/__fixtures__/', `diff2Plain.${extension}`);
     expect(genDiff(previousFile, actualFile)).toBe(plainDiff);
-  });
-
-  test('should work with .yml extension', () => {
-    const previousFile = path.join(__dirname, '/__fixtures__/', 'diff1Plain.yml');
-    const actualFile = path.join(__dirname, '/__fixtures__/', 'diff2Plain.yml');
-    expect(genDiff(previousFile, actualFile)).toBe(plainDiff);
-  });
-});
+  },
+);
--- src/parsers.js
+++ src/parsers.js
@@ -1,19 +1,22 @@
 import path from 'path';
 import fs from 'fs';
 import jsyaml from 'js-yaml';
+import ini from 'ini';
 
 const chooseParser = (extension) => {
   const formatsParsers = {
     '.json': (format) => JSON.parse(format),
     '.yaml': (format) => jsyaml.safeLoad(format),
     '.yml': (format) => jsyaml.safeLoad(format),
+    '.ini': (format) => ini.parse(format),
   };
   return formatsParsers[extension];
 };
 
 export default (filePath) => {
   const fileExtension = path.extname(filePath);
-  const fileData = fs.readFileSync(filePath);
+  const fileData = fs.readFileSync(filePath, 'utf-8');
   const fileParser = chooseParser(fileExtension);
+  if (fileExtension === '.ini') console.log(fileData);
   return fileParser(fileData);
 };
