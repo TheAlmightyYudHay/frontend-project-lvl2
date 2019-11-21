9d88415ce8fe9a5082e400a605542f238303c069
Anton Kulish <antonsergeevichkulish@gmail.com>
2019-11-21 03:54:04 +0200

    Improved code

--- src/formatters/objectFormatter.js
+++ src/formatters/objectFormatter.js
@@ -1,13 +1,12 @@
-import _ from 'lodash';
-
 const prepareValue = (sign, depthOffset) => {
   if (typeof sign !== 'object') return sign;
   const formattedEntries = Object.entries(sign)
-    .reduce((acc, [key, value]) => {
+    .map(([key, value]) => {
       const formattedValue = prepareValue(value, depthOffset + 4);
-      return `${acc}${' '.repeat(depthOffset)}${key}: ${formattedValue}\n`;
-    }, '');
-  return `{\n${_.trimEnd(formattedEntries)}\n${' '.repeat(depthOffset - 4)}}`;
+      return `${' '.repeat(depthOffset)}${key}: ${formattedValue}`;
+    })
+    .join('\n');
+  return `{\n${formattedEntries}\n${' '.repeat(depthOffset - 4)}}`;
 };
 
 const prepareNode = ({
@@ -23,20 +22,20 @@ const prepareNode = ({
 };
 
 const diffRelatedFormats = {
-  unchanged: ({ name, value }, depthOffset) => `${' '.repeat(depthOffset)}${name}: ${value}\n`,
-  added: ({ name, actual }, depthOffset) => `${' '.repeat(depthOffset - 2)}+ ${name}: ${actual}\n`,
-  deleted: ({ name, previous }, depthOffset) => `${' '.repeat(depthOffset - 2)}- ${name}: ${previous}\n`,
-  changed: ({ name, previous, actual }, depthOffset) => `${' '.repeat(depthOffset - 2)}+ ${name}: ${actual}\n${' '.repeat(depthOffset - 2)}- ${name}: ${previous}\n`,
-  nested: (node, depthOffset, handleFn) => `${handleFn(node, depthOffset)}\n`,
+  unchanged: ({ name, value }, depthOffset) => `${' '.repeat(depthOffset)}${name}: ${value}`,
+  added: ({ name, actual }, depthOffset) => `${' '.repeat(depthOffset - 2)}+ ${name}: ${actual}`,
+  deleted: ({ name, previous }, depthOffset) => `${' '.repeat(depthOffset - 2)}- ${name}: ${previous}`,
+  changed: ({ name, previous, actual }, depthOffset) => `${' '.repeat(depthOffset - 2)}+ ${name}: ${actual}\n${' '.repeat(depthOffset - 2)}- ${name}: ${previous}`,
+  nested: (node, depthOffset, handleFn) => handleFn(node, depthOffset),
 };
 
 const objectFormatter = ({ name, children }, depthOffset = 0) => {
   const formattedName = name ? `${name}: ` : '';
-  const formattedChildren = children.reduce(
-    (acc, node) => `${acc}${diffRelatedFormats[node.type](prepareNode(node, depthOffset), depthOffset + 4, objectFormatter)}`,
-    '',
-  );
-  return `${' '.repeat(depthOffset)}${formattedName}{\n${_.trimEnd(formattedChildren)}\n${' '.repeat(depthOffset)}}`;
+  const formattedChildren = children.map((node) => diffRelatedFormats[node.type](
+    prepareNode(node, depthOffset), depthOffset + 4, objectFormatter,
+  ))
+    .join('\n');
+  return `${' '.repeat(depthOffset)}${formattedName}{\n${formattedChildren}\n${' '.repeat(depthOffset)}}`;
 };
 
 export default objectFormatter;
